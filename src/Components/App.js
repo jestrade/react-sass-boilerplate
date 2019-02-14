44910f7adad79077d4c1788bd81d4f4b776cc921
Jesus David Estrada De La Hoz <jesusdavide@CAMPUSUNINORTE.LOCAL>
2019-02-14 18:24:22 -0500

    sass

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,69 +7,74 @@ class App extends Component {
     	  <main>
 
 
-					<header>
+					<header className="header">
 						<nav>
-							<ul>
-								<li><a href="#">Home</a></li>
-								<li><a href="#">Profile</a></li>
-								<li><a href="#">About</a></li>
-								<li><a href="#">Sign out</a></li>
+							<ul className="nav-bar">
+								<li className="nav-item"><a className="nav-link" href="#">Home</a></li>
+								<li className="nav-item"><a className="nav-link" href="#">Profile</a></li>
+								<li className="nav-item"><a className="nav-link" href="#">About</a></li>
+								<li className="nav-item"><a className="nav-link" href="#">Sign out</a></li>
 							</ul>
 						</nav>
 					</header>
-					
-					<section>
-						<h2>¿En qué estás pensando?</h2>
-						<form>
-							<textarea placeholder="¿En qué estás pensando?"></textarea>
-							<button>Tweet this!</button>
-						</form>
-					</section>
-					<section>
-						<h1>Timeline</h1>
-						<ul>
-							<li>
-								<aticle>
-									<h2>@username</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
-									<p>Feb 7, 2014</p>
-									<div></div>
-								</aticle>
-							</li>
-							<li>
-								<aticle>
-									<h2>@username</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
-									<p>Feb 7, 2014</p>
-									<div></div>
-								</aticle>
-							</li>
-							<li>
-								<aticle>
-									<h2>@username</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
-									<p>Feb 7, 2014</p>
-									<div></div>
-								</aticle>
-							</li>
-							<li>
-								<aticle>
-									<h2>@username</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
-									<p>Feb 7, 2019</p>
-									<div></div> 
-								</aticle>
-							</li>
-							<li>
-								<aticle>
-									<h2>@username</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
-									<p>Feb 7, 2014</p>
-									<div></div>
-								</aticle>
-							</li>
-						</ul>
-					</section>
+					<section className="left">
+						<h2>Trend topics sentiment analysis</h2>
+					</section>	
+					<section className="center">
+						<h2>Lorem ipsum</h2>
+						<section>
+							<h2>¿En qué estás pensando?</h2>
+							<form>
+								<textarea placeholder="¿En qué estás pensando?"></textarea>
+								<button>Tweet this!</button>
+							</form>
+						</section>
+						<section>
+							<h1>Timeline</h1>
+							<ul>
+								<li>
+									<article>
+										<h2>@username</h2>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
+										<p>Feb 7, 2014</p>
+										<div></div>
+									</article>
+								</li>
+								<li>
+									<article>
+										<h2>@username</h2>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
+										<p>Feb 7, 2014</p>
+										<div></div>
+									</article>
+								</li>
+								<li>
+									<article>
+										<h2>@username</h2>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
+										<p>Feb 7, 2014</p>
+										<div></div>
+									</article>
+								</li>
+								<li>
+									<article>
+										<h2>@username</h2>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
+										<p>Feb 7, 2019</p>
+										<div></div> 
+									</article>
+								</li>
+								<li>
+									<article>
+										<h2>@username</h2>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
+										<p>Feb 7, 2014</p>
+										<div></div>
+									</article>
+								</li>
+							</ul>
+						</section>
+					</section>	
 					<aside>
 						<section>
 							<h2>Latest tweets</h2>							
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './Components/App'
+import './CSS/style.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'))
